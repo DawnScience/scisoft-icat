692e152ec4d7803897137de2c408363e63118816
Ghita Kouadri Mostefaoui <ghita.mostefaoui@diamond.ac.uk>
2011-12-13 12:09:32 +0000

    ICAT: added listener for ENTER button for login

--- uk.ac.diamond.scisoft.icatexplorer.rcp/src/uk/ac/diamond/scisoft/icatexplorer/rcp/ui/LoginView.java
+++ uk.ac.diamond.scisoft.icatexplorer.rcp/src/uk/ac/diamond/scisoft/icatexplorer/rcp/ui/LoginView.java
@@ -24,6 +24,8 @@ import com.swtdesigner.SWTResourceManager;
 
 import uk.ac.diamond.scisoft.icatexplorer.rcp.icatclient.ICATClient;
 import uk.ac.diamond.scisoft.icatexplorer.rcp.icatclient.ICATSessionDetails;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 
 
 public class LoginView extends ViewPart {
@@ -39,6 +41,7 @@ public class LoginView extends ViewPart {
 	private Text fedidText;
 	private Text passwordText;
 	private Label messageLbl;
+	private Button loginBtn;
 
 	public LoginView() {
 	}
@@ -50,12 +53,23 @@ public class LoginView extends ViewPart {
 	@Override
 	public void createPartControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
+		
 		container.setBackground(com.swtdesigner.SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		
 		fedidText = new Text(container, SWT.BORDER);
 		fedidText.setBounds(98, 71, 138, 19);
 		
 		passwordText = new Text(container, SWT.BORDER);
+		passwordText.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				//logger.debug("key pressed: " + e.keyCode);
+				if(e.keyCode == 13){ //ENTER key pressed
+					logger.debug("ENTER button pressed");
+					login();
+				}
+			}
+		});
 		passwordText.setEchoChar('*');
 		passwordText.setBounds(98, 96, 138, 19);
 		passwordText.setText(password);
@@ -73,75 +87,12 @@ public class LoginView extends ViewPart {
 		passwordLdl.setBounds(10, 96, 66, 13);
 		passwordLdl.setText("Password");
 		
-		Button loginBtn = new Button(container, SWT.NONE);
+		loginBtn = new Button(container, SWT.NONE);
 		loginBtn.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				
-				messageLbl.setText("");
-				
-				// perform icat authentication
-				ICATClient icat = new ICATClient();
-				ICATSessionDetails sessionDetails = new ICATSessionDetails(icat);
-			
-				
-				String fedid    = fedidText.getText();
-				String password = passwordText.getText();
-		    	
-		    	if ( icat.login(fedid, password) != null){
-		    		logger.info("authentication successful!");
-		    		
-		    		fedidText.setEditable(false);
-		    		fedidText.setForeground(com.swtdesigner.SWTResourceManager.getColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND));
-		    		passwordText.setEditable(false);
-		    		passwordText.setForeground(com.swtdesigner.SWTResourceManager.getColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND));
-		    		
-		    		/*
-		    		 * open CNF view
-		    		 * */
-		    		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-		    		try {
-		    		 		    		
-		    		 /*
-		    		  * check first whether an icat content view is already opened
-		    		  */   		
-
-		    		 IViewPart[] viewParts = window.getActivePage().getViews();
-		    		 String cnfViewId = "uk.ac.diamond.scisoft.icatexplorer.rcp.view";
-		    		 for (IViewPart view : viewParts)
-		    		 {
-		    			 if (view.getSite().getId().equalsIgnoreCase(cnfViewId)){
-		    				 // a previous ICAT content view is opened, close it		    				 
-		    				 window.getActivePage().hideView(view);
-		    			 }
-		    		 }
-		    		 
-		    		 // open a fresh icat content view	
-		    		 String cnf = "uk.ac.diamond.scisoft.icatexplorer.rcp.view"; 
-		    		 logger.debug("showing view: " + cnf);
-		    		 window.getActivePage().showView(cnf);
-		    		
-		    		 
-		    		 //close login view
-//		    		 IViewPart[] viewParts = window.getActivePage().getViews();
-//		    		 for (IViewPart view : viewParts)
-//		    		 {
-//		    		 if (view instanceof LoginView)
-//		    		 {
-//		    		 window.getActivePage().hideView(view);
-//		    		 }
-//		    		 }
-    		} catch (PartInitException e1) {
-		    		 // TODO Auto-generated catch block
-		    		 e1.printStackTrace();
-		    		}
-		    		
-		    		
-		    	}else{
-		    		String message = "failed to authenticate, please check your fedid and password!";
-		    		messageLbl.setText(message);
-		    		logger.error(message);
-		    	}    
+				login();
 				
 			}
 		});
@@ -206,5 +157,72 @@ public class LoginView extends ViewPart {
 	@Override
 	public void setFocus() {
 		// Set the focus
+	}
+	
+	public void login(){
+		messageLbl.setText("");
+		
+		// perform icat authentication
+		ICATClient icat = new ICATClient();
+		ICATSessionDetails sessionDetails = new ICATSessionDetails(icat);
+	
+		
+		String fedid    = fedidText.getText();
+		String password = passwordText.getText();
+    	
+    	if ( icat.login(fedid, password) != null){
+    		logger.info("authentication successful!");
+    		
+    		fedidText.setEditable(false);
+    		fedidText.setForeground(com.swtdesigner.SWTResourceManager.getColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND));
+    		passwordText.setEditable(false);
+    		passwordText.setForeground(com.swtdesigner.SWTResourceManager.getColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND));
+    		
+    		/*
+    		 * open CNF view
+    		 * */
+    		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+    		try {
+    		 		    		
+    		 /*
+    		  * check first whether an icat content view is already opened
+    		  */   		
+
+    		 IViewPart[] viewParts = window.getActivePage().getViews();
+    		 String cnfViewId = "uk.ac.diamond.scisoft.icatexplorer.rcp.view";
+    		 for (IViewPart view : viewParts)
+    		 {
+    			 if (view.getSite().getId().equalsIgnoreCase(cnfViewId)){
+    				 // a previous ICAT content view is opened, close it		    				 
+    				 window.getActivePage().hideView(view);
+    			 }
+    		 }
+    		 
+    		 // open a fresh icat content view	
+    		 String cnf = "uk.ac.diamond.scisoft.icatexplorer.rcp.view"; 
+    		 logger.debug("showing view: " + cnf);
+    		 window.getActivePage().showView(cnf);
+    		
+    		 
+    		 //close login view
+//    		 IViewPart[] viewParts = window.getActivePage().getViews();
+//    		 for (IViewPart view : viewParts)
+//    		 {
+//    		 if (view instanceof LoginView)
+//    		 {
+//    		 window.getActivePage().hideView(view);
+//    		 }
+//    		 }
+	} catch (PartInitException e1) {
+    		 // TODO Auto-generated catch block
+    		 e1.printStackTrace();
+    		}
+    		
+    		
+    	}else{
+    		String message = "failed to authenticate, please check your fedid and password!";
+    		messageLbl.setText(message);
+    		logger.error(message);
+    	}
 	}
 }
