08d99ef5514fb1d058fc13f64aa56818b81a053e
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-05-12 11:37:47 +0100

    simplify code

--- uk.ac.diamond.scisoft.icatexplorer.v4.rcp/src/uk/ac/diamond/scisoft/icatexplorer/v4/rcp/launcher/ICATPerspectiveLauncher.java
+++ uk.ac.diamond.scisoft.icatexplorer.v4.rcp/src/uk/ac/diamond/scisoft/icatexplorer/v4/rcp/launcher/ICATPerspectiveLauncher.java
@@ -16,45 +16,14 @@
 
 package uk.ac.diamond.scisoft.icatexplorer.v4.rcp.launcher;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.WorkbenchException;
+import org.dawb.common.ui.perspective.AbstractPerspectiveLaunch;
 
 import uk.ac.diamond.scisoft.icatexplorer.v4.rcp.perspectives.ICATPerspective;
 
-public class ICATPerspectiveLauncher implements IWorkbenchWindowActionDelegate {
+public class ICATPerspectiveLauncher extends AbstractPerspectiveLaunch {
 
 	@Override
-	public void run(IAction action) {
-		try {
-			PlatformUI.getWorkbench().showPerspective(
-					ICATPerspective.PERSPECTIVE_ID,
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-		} catch (WorkbenchException e) {
-			e.printStackTrace();
-		}
-
+	public String getID() {
+		return ICATPerspective.PERSPECTIVE_ID;
 	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void init(IWorkbenchWindow window) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void selectionChanged(IAction arg0, ISelection arg1) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
